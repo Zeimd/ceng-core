281b8604f87f2a231199bea63aa1d5dcd7c99dcc
Zeimd <jv.korkala@gmail.com>
2023-12-30 10:38:55 +0200

    Improvements to operator detection.

--- ceng-glsl/tokenizer.cpp
+++ ceng-glsl/tokenizer.cpp
@@ -37,20 +37,25 @@ Ceng::CRESULT Tokenizer::Tokenize(const Ceng::StringUtf8& fileName, const Ceng::
 	bool startLine = false;
 	bool endLine = false;
 
-	TokenType::value prevTokenType;
-	TokenType::value tokenType = TokenType::meta_uninitialized;
+	bool prevIsOperator = false;
+
+	 
+	TokenType::value prevToken = TokenType::meta_uninitialized;;
+	TokenType::value currentToken = TokenType::meta_uninitialized;;
 
 	for (auto iter = source.ConstBeginIterator(); iter != source.ConstEndIterator(); iter++)
 	{
-		prevTokenType = tokenType;
+		prevToken = currentToken;
+
+		currentToken = TokenType::meta_uninitialized;
 
 		++position;
 
-		char item;
+		char currentChar;
 		
-		(*iter).ToChar(&item);
+		(*iter).ToChar(&currentChar);
 
-		switch (item)
+		switch (currentChar)
 		{
 		case ' ':
 		case '\t':
@@ -75,84 +80,222 @@ Ceng::CRESULT Tokenizer::Tokenize(const Ceng::StringUtf8& fileName, const Ceng::
 			rightSpace = false;
 			continue;
 		case '!':
-			tokenType = TokenType::bang;
-			break;
+			prevIsOperator = true;
+			currentToken = TokenType::bang;
+			continue;
 		case '&':
-			tokenType = TokenType::ampersand;
-			break;
-		case '^':
-			tokenType = TokenType::caret;
-			break;
-		case '*':
-			tokenType = TokenType::star;
-			break;
-		case '/':
-			tokenType = TokenType::slash;
-			break;
-		case '-':
-			tokenType = TokenType::dash;
-			break;
-		case '+':
-			tokenType = TokenType::plus;
-			break;
-		case '%':
-			tokenType = TokenType::percent;
-			break;
-		case '?':
-			tokenType = TokenType::question;
-			break;
-		case ':':
-			tokenType = TokenType::colon;
-			break;
-		case ';':
-			tokenType = TokenType::semicolon;
-			break;
-		case ',':
-			tokenType = TokenType::comma;
-			break;
-		case '.':
-			tokenType = TokenType::dot;
-			break;
-		case '~':
-			tokenType = TokenType::tilde;
-			break;
+			if (prevToken == TokenType::ampersand)
+			{
+				prevIsOperator = false;
+				currentToken = TokenType::and_op;
+				break;
+			}
+			prevIsOperator = true;
+			currentToken = TokenType::ampersand;
+			continue;
 		case '|':
-			tokenType = TokenType::vertical_bar;
-			break;
-		case '(':
-			tokenType = TokenType::left_paren;
-			break;
-		case ')':
-			tokenType = TokenType::right_paren;
-			break;
-		case '[':
-			tokenType = TokenType::left_bracket;
-			break;
-		case ']':
-			tokenType = TokenType::right_bracket;
-			break;
-		case '{':
-			tokenType = TokenType::left_brace;
-			break;
-		case '}':
-			tokenType = TokenType::right_brace;
+			if (prevToken == TokenType::vertical_bar)
+			{
+				prevIsOperator = false;
+				currentToken = TokenType::or_op;
+				break;
+			}
+			prevIsOperator = true;
+			currentToken = TokenType::vertical_bar;
+			continue;
+		case '^':
+			if (prevToken == TokenType::caret)
+			{
+				prevIsOperator = false;
+				currentToken = TokenType::xor_op;
+				break;
+			}
+			prevIsOperator = true;
+			currentToken = TokenType::caret;
+			continue;
+		case '*':
+			if (prevToken == TokenType::slash)
+			{
+				prevIsOperator = false;
+				currentToken = TokenType::preprocess_comment_start;
+				break;
+			}
+			prevIsOperator = true;
+			currentToken = TokenType::star;
+			continue;
+		case '/':
+			if (prevToken == TokenType::slash)
+			{
+				prevIsOperator = false;
+				currentToken = TokenType::preprocess_comment;
+				break;
+			}
+			else if 
+			(prevToken == TokenType::star)
+			{
+				prevIsOperator = false;
+				currentToken = TokenType::preprocess_comment_end;
+				break;
+			}
+			prevIsOperator = true;
+			currentToken = TokenType::slash;
+			continue;
+		case '-':
+			prevIsOperator = true;
+			currentToken = TokenType::dash;
+			continue;
+		case '+':
+			prevIsOperator = true;
+			currentToken = TokenType::plus;
+			continue;
+		case '%':
+			prevIsOperator = true;
+			currentToken = TokenType::percent;
+			continue;
+		case '?':
+			currentToken = TokenType::question;
+			prevIsOperator = true;
+			continue;
+		case ':':
+			if (prevToken == TokenType::question)
+			{
+				prevIsOperator = false;
+				currentToken = TokenType::field_selection;
+				break;
+			}
+			currentToken = TokenType::colon;
 			break;
 		case '<':
-			tokenType = TokenType::left_angle;
-			break;
+			if (prevToken == TokenType::left_angle)
+			{
+				prevIsOperator = false;
+				currentToken = TokenType::left_op;
+				break;
+			}
+			prevIsOperator = true;
+			currentToken = TokenType::left_angle;
+			continue;
 		case '>':
-			tokenType = TokenType::right_angle;
-			break;
+			if (prevToken == TokenType::right_angle)
+			{
+				prevIsOperator = false;
+				currentToken = TokenType::right_op;
+				break;
+			}
+			prevIsOperator = true;
+			currentToken = TokenType::right_angle;
+			continue;
 		case '=':
-			tokenType = TokenType::equal;
+			switch (prevToken)
+			{
+			case TokenType::plus:
+				prevIsOperator = false;
+				currentToken = TokenType::add_assign;
+				break;
+			case TokenType::dash:
+				prevIsOperator = false;
+				currentToken = TokenType::sub_assign;
+				break;
+			case TokenType::star:
+				prevIsOperator = false;
+				currentToken = TokenType::mul_assign;
+				break;
+			case TokenType::slash:
+				prevIsOperator = false;
+				currentToken = TokenType::div_assign;
+				break;
+			case TokenType::percent:
+				prevIsOperator = false;
+				currentToken = TokenType::mod_assign;
+				break;
+			case TokenType::ampersand:
+				prevIsOperator = false;
+				currentToken = TokenType::and_assign;
+				break;
+			case TokenType::vertical_bar:
+				prevIsOperator = false;
+				currentToken = TokenType::or_assign;
+				break;
+			case TokenType::caret:
+				prevIsOperator = false;
+				currentToken = TokenType::xor_assign;
+				break;
+			case TokenType::equal:
+				prevIsOperator = false;
+				currentToken = TokenType::eq_op;
+				break;
+			case TokenType::left_angle:
+				prevIsOperator = false;
+				currentToken = TokenType::le_op;
+				break;
+			case TokenType::right_angle:
+				prevIsOperator = false;
+				currentToken = TokenType::ge_op;
+				break;
+			case TokenType::left_op:
+				prevIsOperator = false;
+				currentToken = TokenType::left_assign;
+				break;
+			case TokenType::right_op:
+				prevIsOperator = false;
+				currentToken = TokenType::right_assign;
+				break;
+			case TokenType::bang:
+				prevIsOperator = false;
+				currentToken = TokenType::ne_op;
+				break;
+			default:
+				prevIsOperator = true;
+				currentToken = TokenType::equal;
+				continue;
+			}
 			break;
+		case ';':
+			currentToken = TokenType::semicolon;
+			break;
+		case ',':
+			currentToken = TokenType::comma;
+			break;
+		case '.':
+			currentToken = TokenType::dot;
+			break;
+		case '~':
+			currentToken = TokenType::tilde;
+			break;
+		case '(':
+			currentToken = TokenType::left_paren;
+			break;
+		case ')':
+			currentToken = TokenType::right_paren;
+			break;
+		case '[':
+			currentToken = TokenType::left_bracket;
+			break;
+		case ']':
+			currentToken = TokenType::right_bracket;
+			break;
+		case '{':
+			currentToken = TokenType::left_brace;
+			break;
+		case '}':
+			currentToken = TokenType::right_brace;
+			break;
+		};
+
+		if (prevIsOperator && prevToken != TokenType::meta_uninitialized)
+		{
+			tokens.emplace_back(sourceFile, line, position, leftSpace, rightSpace, startLine, endLine, prevToken);
 		}
 
+		prevIsOperator = false;
 
-	
+		if (currentToken != TokenType::meta_uninitialized)
+		{
+			tokens.emplace_back(sourceFile, line, position, leftSpace, rightSpace, startLine, endLine, currentToken);
+			continue;
+		}
 
 	}
-
 
 	out_tokens = std::move(tokens);
 
