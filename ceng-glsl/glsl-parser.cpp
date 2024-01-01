124addd358e8884b0b085c10098457675b0cc50f
Zeimd <jv.korkala@gmail.com>
2024-01-01 10:18:37 +0200

    Some parser scetching.

--- ceng-glsl/glsl-parser.cpp
+++ ceng-glsl/glsl-parser.cpp
@@ -6,26 +6,201 @@ CRESULT GLSL_Parser::Parse(const std::vector<Token>& in_tokens)
 {
 	tokens = in_tokens;
 
+	tokenIter = tokens.begin();
+
+	Log("Parsing start");
+
 	void State_Translation_Unit();
+
+	Log("Parsing end");
 
 	return CE_OK;
 }
 
 const Token& GLSL_Parser::PeekToken()
 {
-	return tokens.back();
+	return *tokenIter;
 }
 
 const Token GLSL_Parser::NextToken()
 {
-	Token temp = tokens.back();
+	return (*tokenIter++);
+}
 
-	tokens.pop_back();
+void GLSL_Parser::DiscardNext()
+{
+	++tokenIter;
+}
 
-	return temp;
+const char* GLSL_Parser::GetPrefixText(LogPrefix prefix)
+{
+	switch (prefix)
+	{
+	case log_nominal:
+		return "";
+	case log_error:
+		return "ERROR: ";
+	}
+
+	return "";
+}
+
+
+void GLSL_Parser::Log(const char* text, LogPrefix prefix)
+{
+#ifndef _DEBUG
+	if (prefix == log_debug)
+	{
+		return;
+	}
+#endif
+
+	log += GetPrefixText(prefix);
+	log += text;
+	log += '\n';
+}
+
+void GLSL_Parser::Log(const Ceng::StringUtf8& text, LogPrefix prefix)
+{
+#ifndef _DEBUG
+	if (prefix == log_debug)
+	{
+		return;
+	}
+#endif
+	log += GetPrefixText(prefix);
+	log += text;
+	log += '\n';
+}
+
+
+void GLSL_Parser::LogError(const char* error)
+{
+	Log(error, log_error);
+}
+
+void GLSL_Parser::LogError(const Ceng::StringUtf8& error)
+{
+	Log(error, log_error);
+}
+
+void GLSL_Parser::LogDebug(const char* text)
+{
+	Log(text, log_debug);
+}
+void GLSL_Parser::LogDebug(const Ceng::StringUtf8& text)
+{
+	Log(text, log_debug);
 }
 
 void GLSL_Parser::S_Translation_Unit()
 {
+	Token next = NextToken();
+
+	if (next.type == TokenType::meta_end_of_file)
+	{
+		LogDebug("end of file reached");
+		return;
+	}
+
+	if (next.category == TokenCategory::data_type)
+	{
+		switch (next.type)
+		{
+		case TokenType::type_name:
+			S_TU_TypeSpecNoArr(TypeSpecifierNoArray(next.name));
+			break;
+		default:
+			S_TU_TypeSpecNoArr(TypeSpecifierNoArray(next.type));
+			break;
+		}
+	}
+	else
+	{
+		Ceng::StringUtf8 text;
+		text = "no grammar for token: ";
+		text += next.ToString();
+		LogError(text);
+		/*
+		switch (next.type)
+		{
+		case TokenType::keyword_const:
+			S_TU_StorageQ(StorageQualifier::sq_const);
+			break;
+		case TokenType::keyword_attribute:
+			S_TU_StorageQ(StorageQualifier::sq_attribute);
+			break;
+		case TokenType::keyword_varying:
+			S_TU_StorageQ(StorageQualifier::sq_varying);
+			break;
+		case TokenType::keyword_uniform:
+			S_TU_StorageQ(StorageQualifier::sq_uniform);
+			break;
+		case TokenType::keyword_in:
+			S_TU_StorageQ(StorageQualifier::sq_in);
+			break;
+		case TokenType::keyword_out:
+			S_TU_StorageQ(StorageQualifier::sq_out);
+			break;
+		case TokenType::keyword_centroid:
+			switch (PeekToken().type)
+			{
+			case TokenType::keyword_in:
+				S_TU_StorageQ(StorageQualifier::sq_centroid_in);
+				break;
+			case TokenType::keyword_out:
+				S_TU_StorageQ(StorageQualifier::sq_centroid_out);
+				break;
+			case TokenType::keyword_varying:
+				S_TU_StorageQ(StorageQualifier::sq_centroid_varying);
+				break;
+			default:
+				Ceng::StringUtf8 text;
+				text = next.ToString();
+				text += " not allowed after centroid";
+				LogError(text);
+				break;
+			}
+			break;
+		}
+		*/
+	}
+}
+
+void GLSL_Parser::S_TU_TypeSpecNoArr(const TypeSpecifierNoArray& typeSpec)
+{
+	Token next = NextToken();
+
+	if (next.type == TokenType::left_bracket)
+	{
+		S_TU_TypeSpecNoArr_LB(typeSpec);
+	}
+	else if (next.type != TokenType::left_bracket)
+	{
+		S_TU_TypeSpecNoPrec(typeSpec);
+	}
+	else
+	{
+		Ceng::StringUtf8 text;
+		text += next.ToString();
+		text += " not allowed after typename";
+
+		LogError(text);
+	}
+}
+
+void GLSL_Parser::S_TU_TypeSpecNoArr_LB(const TypeSpecifierNoArray& typeSpec)
+{
 
 }
+
+void GLSL_Parser::S_TU_TypeSpecNoPrec(const TypeSpecifierNoArray& typeSpec)
+{
+
+}
+
+void GLSL_Parser::S_TU_StorageQ(StorageQualifier::value sq)
+{
+
+}
+
--- ceng-glsl/inc/ceng/glsl-parser.h
+++ ceng-glsl/inc/ceng/glsl-parser.h
@@ -6,6 +6,9 @@
 #include <vector>
 #include "token.h"
 
+#include "storage-qualifier.h"
+#include "type-spec-no-arr.h"
+
 namespace Ceng
 {
 	class GLSL_Parser
@@ -13,6 +16,17 @@ namespace Ceng
 	public:
 
 		std::vector<Token> tokens;
+
+		std::vector<Token>::const_iterator tokenIter;
+
+		Ceng::StringUtf8 log;
+
+		enum LogPrefix
+		{
+			log_nominal,
+			log_error,
+			log_debug,
+		};
 
 	public:
 		
@@ -22,7 +36,30 @@ namespace Ceng
 
 		const Token NextToken();
 
+		// Used when PeekToken() has been used to deduce next parser action
+		void DiscardNext();
+
+		void Log(const char* text, LogPrefix prefix=log_nominal);
+		void Log(const Ceng::StringUtf8& text, LogPrefix prefix = log_nominal);
+
+		void LogError(const char* error);
+		void LogError(const char* fmt_str,...);
+		void LogError(const Ceng::StringUtf8& error);
+
+		void LogDebug(const char* error);
+		void LogDebug(const Ceng::StringUtf8& error);
+
+		static const char* GetPrefixText(LogPrefix prefix);
+
+	protected:
+
 		void S_Translation_Unit();
+		void S_TU_TypeSpecNoArr(const TypeSpecifierNoArray& typeSpec);
+
+		void S_TU_TypeSpecNoArr_LB(const TypeSpecifierNoArray& typeSpec);
+
+		void S_TU_TypeSpecNoPrec(const TypeSpecifierNoArray& typeSpec);
+		void S_TU_StorageQ(StorageQualifier::value sq);
 	};
 }
 
