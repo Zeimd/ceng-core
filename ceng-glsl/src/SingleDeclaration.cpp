ddcab825970d44db9e309e90d33d43d33a6e0f89
Zeimd <jv.korkala@gmail.com>
2024-01-14 16:53:29 +0200

    Add constructors.

--- ceng-glsl/src/SingleDeclaration.cpp
+++ ceng-glsl/src/SingleDeclaration.cpp
@@ -2,8 +2,45 @@
 
 using namespace Ceng;
 
+SingleDeclaration::SingleDeclaration(const FullySpecifiedType& fullType)
+	: INonTerminal(NonTerminalType::single_declaration), fullType(fullType), hasName(false),invariant(false),
+	arraySizeExpression(nullptr), initializer(nullptr)
+{
+
+}
+
 SingleDeclaration::SingleDeclaration(const FullySpecifiedType& fullType, const Ceng::StringUtf8& name)
-	: INonTerminal(NonTerminalType::single_declaration), fullType(fullType), name(name), initializer()
+	: INonTerminal(NonTerminalType::single_declaration), fullType(fullType), hasName(true), invariant(false),
+	arraySizeExpression(nullptr), initializer(nullptr)
+{
+
+}
+
+SingleDeclaration::SingleDeclaration(const FullySpecifiedType& fullType, const Ceng::StringUtf8& name, std::shared_ptr<Initializer>& initializer)
+	: INonTerminal(NonTerminalType::single_declaration), fullType(fullType), hasName(true), invariant(false),
+	arraySizeExpression(nullptr), initializer(initializer)
+{
+
+}
+
+SingleDeclaration::SingleDeclaration(const FullySpecifiedType& fullType, const Ceng::StringUtf8& name, std::shared_ptr<ConstantExpression>& arraySizeExpression)
+	: INonTerminal(NonTerminalType::single_declaration), fullType(fullType), hasName(true), invariant(false),
+	arraySizeExpression(arraySizeExpression), initializer(nullptr)
+{
+
+}
+
+SingleDeclaration::SingleDeclaration(const FullySpecifiedType& fullType, const Ceng::StringUtf8& name, std::shared_ptr<ConstantExpression>& arraySizeExpression,
+	std::shared_ptr<Initializer>& initializer)
+	: INonTerminal(NonTerminalType::single_declaration), fullType(fullType), hasName(true), invariant(false),
+	arraySizeExpression(arraySizeExpression), initializer(initializer)
+{
+
+}
+
+SingleDeclaration::SingleDeclaration(const Ceng::StringUtf8& name)
+	: INonTerminal(NonTerminalType::single_declaration), fullType(fullType), hasName(true), invariant(true),
+	arraySizeExpression(nullptr), initializer(nullptr)
 {
 
 }
--- ceng-glsl/src/SingleDeclaration.h
+++ ceng-glsl/src/SingleDeclaration.h
@@ -13,20 +13,37 @@ namespace Ceng
 	public:
 		FullySpecifiedType fullType;
 
+		bool hasName;
 		Ceng::StringUtf8 name;
 
-		bool isArray;
-		ConstantExpression arraySizeExpression;
+		std::shared_ptr<ConstantExpression> arraySizeExpression;
 
 		bool invariant;
 
-		Initializer initializer;
+		std::shared_ptr<Initializer> initializer;
 
 	public:
 
 		void Release() override;
 
+		// fully_specified_type
+		SingleDeclaration(const FullySpecifiedType& fullType);
+
+		// fully_specified_type IDENTIFIER
 		SingleDeclaration(const FullySpecifiedType& fullType, const Ceng::StringUtf8& name);
+
+		// fully_specified_type IDENTIFIER EQUAL initializer
+		SingleDeclaration(const FullySpecifiedType& fullType, const Ceng::StringUtf8& name, std::shared_ptr<Initializer>& initializer);
+
+		// fully_specified_type IDENTIFIER LEFT_BRACKET {constant_expression} RIGHT_BRACKET
+		SingleDeclaration(const FullySpecifiedType& fullType, const Ceng::StringUtf8& name, std::shared_ptr<ConstantExpression>& arraySizeExpression);
+
+		// fully_specified_type IDENTIFIER LEFT_BRACKET {constant_expression} RIGHT_BRACKET EQUAL initializer
+		SingleDeclaration(const FullySpecifiedType& fullType, const Ceng::StringUtf8& name, std::shared_ptr<ConstantExpression>& arraySizeExpression,
+			std::shared_ptr<Initializer>& initializer);
+
+		// INVARIANT IDENTIFIER
+		SingleDeclaration(const Ceng::StringUtf8& name);
 	};
 }
 
