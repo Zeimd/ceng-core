732a2b92543724b694e2eb42c1fcb2c1e86b9171
Zeimd <jv.korkala@gmail.com>
2024-03-31 11:45:15 +0300

    Add parameter qualifier non-terminal.

---
+++ ceng-glsl/src/ParameterQualifier.cpp
@@ -0,0 +1,55 @@
+#include <ceng/GLSL/Token.h>
+
+#include "ParameterQualifier.h"
+
+using namespace Ceng;
+
+ParameterQualifier::~ParameterQualifier()
+{
+
+}
+
+void ParameterQualifier::Release()
+{
+	delete this;
+}
+
+ParameterQualifier::ParameterQualifier(const Token& token)
+	: INonTerminal(NonTerminalType::parameter_qualifier)
+{
+	type = TokenToType(token);
+}
+
+ParameterQualifier::ParameterQualifier(ParameterQualifierType::value type)
+	: INonTerminal(NonTerminalType::parameter_qualifier), type(type)
+{
+
+}
+
+Ceng::StringUtf8 ParameterQualifier::ToString() const
+{
+	switch (type)
+	{
+	case ParameterQualifierType::in:
+		return "in";
+	case ParameterQualifierType::out:
+		return "out";
+	case ParameterQualifierType::inout:
+		return "inout";
+	default:
+		return "<UNHANDLED PARAMETER QUALIFIER TYPE>";
+	}
+}
+
+ParameterQualifierType::value ParameterQualifier::TokenToType(const Token& token)
+{
+	switch (token.type)
+	{
+	case TokenType::keyword_in:
+		return ParameterQualifierType::in;
+	case TokenType::keyword_out:
+		return ParameterQualifierType::out;
+	case TokenType::keyword_inout:
+		return ParameterQualifierType::inout;
+	}
+}
---
+++ ceng-glsl/src/ParameterQualifier.h
@@ -0,0 +1,42 @@
+#ifndef CENG_GLSL_PARAMETER_QUALIFIER_H
+#define CENG_GLSL_PARAMETER_QUALIFIER_H
+
+#include "INonTerminal.h"
+
+namespace Ceng
+{
+	namespace ParameterQualifierType
+	{
+		enum value
+		{
+			in,
+			out,
+			inout,
+		};
+	}
+
+	class Token;
+
+	class ParameterQualifier : public INonTerminal
+	{
+	public:
+
+		ParameterQualifierType::value type;
+
+	public:
+
+		~ParameterQualifier() override;
+
+		void Release() override;
+
+		ParameterQualifier(const Token& token);
+
+		ParameterQualifier(ParameterQualifierType::value type);
+	
+		Ceng::StringUtf8 ToString() const override;
+
+		static ParameterQualifierType::value TokenToType(const Token& token);
+	};
+}
+
+#endif
