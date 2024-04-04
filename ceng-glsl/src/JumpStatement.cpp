0c4d289fb256acc0f391222e1f844e1dd4d8adfc
Zeimd <jv.korkala@gmail.com>
2024-04-04 18:11:33 +0300

    Add jump statement non-terminal.

---
+++ ceng-glsl/src/JumpStatement.cpp
@@ -0,0 +1,66 @@
+#include "JumpStatement.h"
+#include "Expression.h"
+
+using namespace Ceng;
+
+JumpStatement::~JumpStatement()
+{
+
+}
+
+void JumpStatement::Release()
+{
+	delete this;
+}
+
+JumpStatement::JumpStatement(JumpType::value jumpType)
+	: INonTerminal(NonTerminalType::jump_statement), jumpType(jumpType), returnExpression(nullptr)
+{
+
+}
+
+JumpStatement::JumpStatement(std::shared_ptr<Expression>& returnExpression)
+	: INonTerminal(NonTerminalType::jump_statement), jumpType(JumpType::returnStatement), returnExpression(returnExpression)
+{
+
+}
+
+Ceng::StringUtf8 JumpStatement::ToString(unsigned int indentLevel) const
+{
+	Ceng::StringUtf8 out;
+
+	out += GetIndent(indentLevel);
+
+	switch (jumpType)
+	{
+	case JumpType::breakStatement:
+		out += "break;";
+		break;
+	case JumpType::continueStatement:
+		out += "continue;";
+		break;
+	case JumpType::discard:
+		out += "discard;";
+		break;
+	case JumpType::returnStatement:
+
+		if (returnExpression == nullptr)
+		{
+			out += "return;";
+		}
+		else
+		{
+			out += "return ";
+			out += returnExpression->ToString(indentLevel);
+			out += ";";
+		}
+		
+
+		break;
+		
+	default:
+		out += "<UNHANDLED JUMP TYPE>";
+	}
+
+	return out;
+}
--- ceng-glsl/src/JumpStatement.h
+++ ceng-glsl/src/JumpStatement.h
@@ -5,9 +5,36 @@
 
 namespace Ceng
 {
+	namespace JumpType
+	{
+		enum value
+		{
+			breakStatement,
+			continueStatement,
+			discard,
+			returnStatement,
+		};
+	}
+
+	class Expression;
+
 	class JumpStatement : public INonTerminal
 	{
 	public:
+
+		JumpType::value jumpType;
+
+		std::shared_ptr<Expression> returnExpression;
+
+	public:
+
+		~JumpStatement() override;
+
+		void Release() override;
+
+		JumpStatement(JumpType::value jumpType);
+
+		JumpStatement(std::shared_ptr<Expression>& returnExpression);
 
 		Ceng::StringUtf8 ToString(unsigned int indentLevel) const override;
 	};
