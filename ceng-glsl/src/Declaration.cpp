52b16dca25b3698e77c933ceb5ae48026cd62bed
Zeimd <jv.korkala@gmail.com>
2024-04-06 19:25:40 +0300

    Update non-terminal implementation with all construction types.

--- ceng-glsl/src/Declaration.cpp
+++ ceng-glsl/src/Declaration.cpp
@@ -1,11 +1,27 @@
 #include "Declaration.h"
 #include "FunctionPrototype.h"
 #include "InitDeclaratorList.h"
+#include "StructDeclarationList.h"
+#include "Expression.h"
+#include "TypeQualifier.h"
+#include "TypeSpecifierNoPrecision.h"
+#include "PrecisionQualifier.h"
 
 using namespace Ceng;
 
+Declaration::~Declaration()
+{
+
+}
+
+void Declaration::Release()
+{
+	delete this;
+}
+
 Declaration::Declaration(std::shared_ptr<InitDeclaratorList>& declList)
-	: INonTerminal(NonTerminalType::declaration), type(DeclarationType::init_list),declList(declList),prototype(nullptr)
+	: INonTerminal(NonTerminalType::declaration), type(DeclarationType::init_list),declList(declList),prototype(nullptr),
+	arraySize(nullptr), implicitArray(false)
 {
 
 }
@@ -16,29 +32,117 @@ Declaration::Declaration(std::shared_ptr<FunctionPrototype>& prototype)
 
 }
 
-void Declaration::Release()
+Declaration::Declaration(std::shared_ptr<PrecisionQualifier>& precision, std::shared_ptr<TypeSpecifierNoPrec>& spec)
+	: INonTerminal(NonTerminalType::declaration), type(DeclarationType::precision), precision(precision), spec(spec)
 {
-	delete this;
+
 }
+
+Declaration::Declaration(std::shared_ptr<TypeQualifier>& typeQ, const Ceng::StringUtf8& structName, std::shared_ptr<StructDeclarationList>& list)
+	: INonTerminal(NonTerminalType::declaration), type(DeclarationType::interface_block_define), typeQ(typeQ), interfaceName(interfaceName), list(list)
+{
+
+}
+
+Declaration::Declaration(std::shared_ptr<TypeQualifier>& typeQ, const Ceng::StringUtf8& structName, std::shared_ptr<StructDeclarationList>& list,
+	const Ceng::StringUtf8& variableName)
+	: INonTerminal(NonTerminalType::declaration), type(DeclarationType::interface_block), typeQ(typeQ), interfaceName(interfaceName), list(list), variableName(variableName)
+{
+
+}
+
+Declaration::Declaration(std::shared_ptr<TypeQualifier>& typeQ, const Ceng::StringUtf8& interfaceName, std::shared_ptr<StructDeclarationList>& list,
+	const Ceng::StringUtf8& variableName, bool implicitArray)
+	: INonTerminal(NonTerminalType::declaration), type(DeclarationType::interface_block_array), typeQ(typeQ), interfaceName(interfaceName), list(list), variableName(variableName),
+	implicitArray(true)
+{
+
+}
+
+Declaration::Declaration(std::shared_ptr<TypeQualifier>& typeQ, const Ceng::StringUtf8& interfaceName, std::shared_ptr<StructDeclarationList>& list,
+	const Ceng::StringUtf8& variableName, std::shared_ptr<Expression>& arraySize)
+	: INonTerminal(NonTerminalType::declaration), type(DeclarationType::interface_block_array), typeQ(typeQ), interfaceName(interfaceName), list(list), variableName(variableName),
+	implicitArray(false), arraySize(arraySize)
+{
+
+}
+
+Declaration::Declaration(std::shared_ptr<TypeQualifier>& typeQ)
+	: INonTerminal(NonTerminalType::declaration), type(DeclarationType::type_qualifier), typeQ(typeQ)
+{
+
+}
+
 
 Ceng::StringUtf8 Declaration::ToString(unsigned int indentLevel) const
 {
-	Ceng::StringUtf8 temp;
+	Ceng::StringUtf8 out;
 
 	switch (type)
 	{
 	case DeclarationType::function_prototype:
-		temp = prototype->ToString(indentLevel);
+		out = prototype->ToString(indentLevel);
 		break;
 	case DeclarationType::init_list:
-		temp = declList->ToString(indentLevel);
+		out = declList->ToString(indentLevel);
+		break;
+	case DeclarationType::precision:
+		out += "precision ";
+		out += precision->ToString(indentLevel);
+		out += ' ';
+		out += spec->ToString(indentLevel);
+		break;
+	case DeclarationType::interface_block_define:
+		out += typeQ->ToString(indentLevel);
+		out += ' ';
+		out += interfaceName;
+		out += '\n';
+		out += GetIndent(indentLevel);
+		out += "{\n";
+		out += list->ToString(indentLevel+1);
+
+		out += GetIndent(indentLevel);
+		out += "}";
+		break;
+	case DeclarationType::interface_block:
+		out += typeQ->ToString(indentLevel);
+		out += ' ';
+		out += interfaceName;
+		out += '\n';
+		out += GetIndent(indentLevel);
+		out += "{\n";
+		out += list->ToString(indentLevel + 1);
+
+		out += GetIndent(indentLevel);
+		out += "} ";
+		out + variableName;
+		break;
+	case DeclarationType::interface_block_array:
+		out += typeQ->ToString(indentLevel);
+		out += ' ';
+		out += interfaceName;
+		out += '\n';
+		out += GetIndent(indentLevel);
+		out += "{\n";
+		out += list->ToString(indentLevel + 1);
+
+		out += GetIndent(indentLevel);
+		out += "} ";
+		out + variableName;
+		out += '[';
+
+		if (implicitArray == false)
+		{
+			out += arraySize->ToString(indentLevel);
+		}
+
+		out += ']';
 		break;
 	default:
 		return "<UNHANDLED DECLARATION TYPE>";
 	}
-
 	
-	temp += ';';
+	out += ';';
 
-	return temp;
+	return out;
 }
--- ceng-glsl/src/Declaration.h
+++ ceng-glsl/src/Declaration.h
@@ -13,11 +13,21 @@ namespace Ceng
 		{
 			function_prototype,
 			init_list,
+			precision,
+			interface_block_define,
+			interface_block,
+			interface_block_array,
+			type_qualifier,
 		};
 	}
 
 	class InitDeclaratorList;
 	class FunctionPrototype;
+	class PrecisionQualifier;
+	class TypeSpecifierNoPrec;
+	class StructDeclarationList;
+	class TypeQualifier;
+	class Expression;
 
 	class Declaration : public INonTerminal
 	{
@@ -29,13 +39,45 @@ namespace Ceng
 
 		std::shared_ptr<InitDeclaratorList> declList;
 
+		std::shared_ptr<PrecisionQualifier> precision;
+		std::shared_ptr<TypeSpecifierNoPrec> spec;
+
+		std::shared_ptr<TypeQualifier> typeQ;
+
+		Ceng::StringUtf8 interfaceName;
+
+		std::shared_ptr<StructDeclarationList> list;
+
+		Ceng::StringUtf8 variableName;
+
+		bool implicitArray;
+
+		std::shared_ptr<Expression> arraySize;
+
 	public:
+
+		~Declaration() override;
 
 		void Release() override;
 
 		Declaration(std::shared_ptr<FunctionPrototype>& prototype);
 
 		Declaration(std::shared_ptr<InitDeclaratorList>& declList);
+
+		Declaration(std::shared_ptr<PrecisionQualifier>& precision, std::shared_ptr<TypeSpecifierNoPrec>& spec);
+
+		Declaration(std::shared_ptr<TypeQualifier>& typeQ, const Ceng::StringUtf8& interfaceName, std::shared_ptr<StructDeclarationList>& list);
+
+		Declaration(std::shared_ptr<TypeQualifier>& typeQ, const Ceng::StringUtf8& interfaceName, std::shared_ptr<StructDeclarationList>& list,
+			const Ceng::StringUtf8& variableName);
+
+		Declaration(std::shared_ptr<TypeQualifier>& typeQ, const Ceng::StringUtf8& interfaceName, std::shared_ptr<StructDeclarationList>& list,
+			const Ceng::StringUtf8& variableName, bool implicitArray);
+
+		Declaration(std::shared_ptr<TypeQualifier>& typeQ, const Ceng::StringUtf8& interfaceName, std::shared_ptr<StructDeclarationList>& list,
+			const Ceng::StringUtf8& variableName, std::shared_ptr<Expression>& arraySize);
+
+		Declaration(std::shared_ptr<TypeQualifier>& typeQ);
 
 		Ceng::StringUtf8 ToString(unsigned int indentLevel) const override;
 
