8fa45d230cfbffe3c8f8e8492bed2030cf4c7a26
Zeimd <jv.korkala@gmail.com>
2024-04-13 10:03:48 +0300

    Add shorthand constructors.

--- ceng-glsl/src/AST_Datatype.cpp
+++ ceng-glsl/src/AST_Datatype.cpp
@@ -8,13 +8,26 @@ AST_Datatype::AST_Datatype()
 
 }
 
+AST_Datatype::AST_Datatype(BasicType::value basicType)
+	: category(TypenameCategory::basic_type), basicType(basicType), index(false)
+{
+
+}
+
 AST_Datatype::AST_Datatype(BasicType::value basicType, ArrayIndex& index)
 	: category(TypenameCategory::basic_type), basicType(basicType),index(index)
 {
 
 }
 
-AST_Datatype::AST_Datatype(const Ceng::StringUtf8& customTypeName, ArrayIndex& index)
+AST_Datatype::AST_Datatype(Ceng::StringUtf8& customTypeName)
+	: category(TypenameCategory::custom_type), customTypeName(customTypeName), index(false)
+{
+
+}
+
+
+AST_Datatype::AST_Datatype(Ceng::StringUtf8& customTypeName, ArrayIndex& index)
 	: category(TypenameCategory::custom_type), customTypeName(customTypeName), index(index)
 {
 
