51f10f383ccc980912b6676ee1c7cae17a00b5aa
Zeimd <jv.korkala@gmail.com>
2024-04-15 09:02:15 +0300

    Move enum to separate header.

--- ceng-glsl/inc/ceng/GLSL/AST_BinaryOperation.h
+++ ceng-glsl/inc/ceng/GLSL/AST_BinaryOperation.h
@@ -6,47 +6,12 @@
 #include "Lvalue.h"
 #include "Rvalue.h"
 
+#include "AST_BinaryOperator.h"
+
 namespace Ceng
 {
 	namespace GLSL
 	{
-		namespace BinaryOperator
-		{
-			enum value
-			{
-				add,
-				sub,
-
-				mul,
-				div,
-				mod,
-
-				left_shift,
-				right_shift,
-
-				bitwise_and,
-				bitwise_or,
-				bitwise_xor,
-
-				less,
-				less_eq,
-
-				greater,
-				greater_eq,
-
-				equal,
-				not_equal,
-
-				field_select,
-
-				logical_and,
-				logical_or,
-				logical_xor,
-			};
-
-			const char* ToString(BinaryOperator::value operation);
-		}
-
 		class AST_BinaryOperation : public IASTNode
 		{
 		public:
---
+++ ceng-glsl/inc/ceng/GLSL/AST_BinaryOperator.h
@@ -0,0 +1,47 @@
+#ifndef CENG_GLSL_AST_BINARYOPERATOR_H
+#define CENG_GLSL_AST_BINARYOPERATOR_H
+
+namespace Ceng
+{
+	namespace GLSL
+	{
+		namespace BinaryOperator
+		{
+			enum value
+			{
+				add,
+				sub,
+
+				mul,
+				div,
+				mod,
+
+				left_shift,
+				right_shift,
+
+				bitwise_and,
+				bitwise_or,
+				bitwise_xor,
+
+				less,
+				less_eq,
+
+				greater,
+				greater_eq,
+
+				equal,
+				not_equal,
+
+				field_select,
+
+				logical_and,
+				logical_or,
+				logical_xor,
+			};
+
+			const char* ToString(BinaryOperator::value operation);
+		}
+	}
+}
+
+#endif
