3fd73205c8c78de97237276b48ce3e911657ab1a
Zeimd <jv.korkala@gmail.com>
2025-05-15 19:51:25 +0300

    Add files in preparation for class.

---
+++ sw_renderer/PixelShaderInstanceCommon.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef CENG_SWR_PIXEL_SHADER_INSTANCE_COMMON_H
+#define CENG_SWR_PIXEL_SHADER_INSTANCE_COMMON_H
+
+namespace Ceng
+{
+	class PixelShaderInstanceCommon
+	{
+	public:
+
+	
+
+	};
+}
+
+#endif
