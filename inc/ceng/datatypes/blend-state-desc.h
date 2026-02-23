63cec5aba196798ca5706ccd75fa65c261eaa8ae
Zeimd <jv.korkala@gmail.com>
2026-02-23 22:05:58 +0200

    Add comments.

--- inc/ceng/datatypes/blend-state-desc.h
+++ inc/ceng/datatypes/blend-state-desc.h
@@ -17,29 +17,56 @@ namespace Ceng
 	{
 		enum value
 		{
+			// blend factor = (0,0,0,0)
 			zero = 1,
+
+			// blend factor = (1,1,1,1)
 			one = 2 ,
 
+			// blend factor = (sourceA, sourceB, sourceG, sourceR)
 			source_color = 3 ,
+
+			// blend factor = (1 - sourceA, 1 - sourceB, 1 - sourceG, 1 - sourceR)
 			invert_source_color = 4 ,
 
+			// blend factor = (sourceA, sourceA, sourceA, sourceA)
 			source_alpha = 5 ,
+
+			// blend factor = (1 - sourceA, 1 - sourceA, 1 - sourceA, 1 - sourceA)
 			invert_source_alpha = 6 ,
 
+			// blend factor = (destA, destA, destA, destA)
 			dest_alpha = 7 ,
+
+			// blend factor = (1 - destA, 1 - destA, 1 - destA, 1 - destA)
 			invert_dest_alpha = 8 ,
 
+			// blend factor = (destA, destB, destG, destR)
 			dest_color = 9 ,
+
+			// blend factor = (1 - destA, 1 - destB, 1 - destG, 1 - destR)
 			invert_dest_color = 10  ,
 
+			// blend factor = (1, f, f, f), f = min(sourceA, 1 - destA). Data clamped to [0,1]
 			source_alpha_saturate = 11 ,
 
+			// blend factor set via API function SetBlendState()
 			blend_factor = 12 ,
 
+			// blend factor set via API function SetBlendState(), but inverted 1-x.
 			invert_blend_factor = 13 ,
 
+			// blend factor = (secA, secB, secG, secR)
+			second_source_color = 14,
 
+			// blend factor = (1 - secA, 1 - secB, 1 - secG, 1 - secR)
+			invert_second_source_color = 15,
 
+			// blend factor = (secA, secA, secA, secA)
+			second_source_alpha = 16,
+
+			// blend factor = (1 - secA, 1 - secA, 1 - secA, 1 - secA)
+			invert_second_source_alpha = 17,
 		};
 	}
 
@@ -47,10 +74,19 @@ namespace Ceng
 	{
 		enum value
 		{
+			// source1 + source2
 			add = 1 ,
+
+			// source1 - source2
 			subtract = 2 ,
+
+			// source2 - source1
 			reverse_subtract = 3 ,
+
+			// min(source1,source2). Invalid when using second source blend modes.
 			min = 4 ,
+
+			// max(source1, source2). Invalid when using second source blend modes.
 			max = 5 ,
 		};
 	}
