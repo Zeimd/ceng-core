3ba08f9c84f8725a79dcfb7174c6c1214d59fdd2
Zeimd <jv.korkala@gmail.com>
2026-03-11 17:44:01 +0200

    Change enums so that they start running from zero.

--- inc/ceng/datatypes/blend-state-desc.h
+++ inc/ceng/datatypes/blend-state-desc.h
@@ -18,55 +18,55 @@ namespace Ceng
 		enum value
 		{
 			// blend factor = (0,0,0,0)
-			zero = 1,
+			zero = 0,
 
 			// blend factor = (1,1,1,1)
-			one = 2 ,
+			one = 1 ,
 
 			// blend factor = (sourceA, sourceB, sourceG, sourceR)
-			source_color = 3 ,
+			source_color = 2 ,
 
 			// blend factor = (1 - sourceA, 1 - sourceB, 1 - sourceG, 1 - sourceR)
-			invert_source_color = 4 ,
+			invert_source_color = 3 ,
 
 			// blend factor = (sourceA, sourceA, sourceA, sourceA)
-			source_alpha = 5 ,
+			source_alpha = 4 ,
 
 			// blend factor = (1 - sourceA, 1 - sourceA, 1 - sourceA, 1 - sourceA)
-			invert_source_alpha = 6 ,
+			invert_source_alpha = 5 ,
 
 			// blend factor = (destA, destA, destA, destA)
-			dest_alpha = 7 ,
+			dest_alpha = 6 ,
 
 			// blend factor = (1 - destA, 1 - destA, 1 - destA, 1 - destA)
-			invert_dest_alpha = 8 ,
+			invert_dest_alpha = 7 ,
 
 			// blend factor = (destA, destB, destG, destR)
-			dest_color = 9 ,
+			dest_color = 8 ,
 
 			// blend factor = (1 - destA, 1 - destB, 1 - destG, 1 - destR)
-			invert_dest_color = 10  ,
+			invert_dest_color = 9  ,
 
 			// blend factor = (1, f, f, f), f = min(sourceA, 1 - destA). Data clamped to [0,1]
-			source_alpha_saturate = 11 ,
+			source_alpha_saturate = 10 ,
 
 			// blend factor set via API function SetBlendState()
-			blend_factor = 12 ,
+			blend_factor = 11 ,
 
 			// blend factor set via API function SetBlendState(), but inverted 1-x.
-			invert_blend_factor = 13 ,
+			invert_blend_factor = 12 ,
 
 			// blend factor = (secA, secB, secG, secR)
-			second_source_color = 14,
+			second_source_color = 13,
 
 			// blend factor = (1 - secA, 1 - secB, 1 - secG, 1 - secR)
-			invert_second_source_color = 15,
+			invert_second_source_color = 14,
 
 			// blend factor = (secA, secA, secA, secA)
-			second_source_alpha = 16,
+			second_source_alpha = 15,
 
 			// blend factor = (1 - secA, 1 - secA, 1 - secA, 1 - secA)
-			invert_second_source_alpha = 17,
+			invert_second_source_alpha = 16,
 		};
 	}
 
@@ -75,37 +75,37 @@ namespace Ceng
 		enum value
 		{
 			// blend factor = (0,0,0,0)
-			zero = 1,
+			zero = 0,
 
 			// blend factor = (1,1,1,1)
-			one = 2,
+			one = 1,
 
 			// blend factor = (sourceA, sourceA, sourceA, sourceA)
-			source_alpha = 3,
+			source_alpha = 2,
 
 			// blend factor = (1 - sourceA, 1 - sourceA, 1 - sourceA, 1 - sourceA)
-			invert_source_alpha = 4,
+			invert_source_alpha = 3,
 
 			// blend factor = (destA, destA, destA, destA)
-			dest_alpha = 5,
+			dest_alpha = 4,
 
 			// blend factor = (1 - destA, 1 - destA, 1 - destA, 1 - destA)
-			invert_dest_alpha = 6,
+			invert_dest_alpha = 5,
 
 			// blend factor = (1, f, f, f), f = min(sourceA, 1 - destA). Data clamped to [0,1]
-			source_alpha_saturate = 7,
+			source_alpha_saturate = 6,
 
 			// blend factor set via API function SetBlendState()
-			blend_factor = 8,
+			blend_factor = 7,
 
 			// blend factor set via API function SetBlendState(), but inverted 1-x.
-			invert_blend_factor = 9,
+			invert_blend_factor = 8,
 
 			// blend factor = (secA, secA, secA, secA)
-			second_source_alpha = 10,
+			second_source_alpha = 9,
 
 			// blend factor = (1 - secA, 1 - secA, 1 - secA, 1 - secA)
-			invert_second_source_alpha = 11,
+			invert_second_source_alpha = 10,
 		};
 	}
 
@@ -114,19 +114,19 @@ namespace Ceng
 		enum value
 		{
 			// source1 + source2
-			add = 1 ,
+			add = 0 ,
 
 			// source1 - source2
-			subtract = 2 ,
+			subtract = 1 ,
 
 			// source2 - source1
-			reverse_subtract = 3 ,
+			reverse_subtract = 2 ,
 
 			// min(source1,source2). Invalid when using second source blend modes.
-			min = 4 ,
+			min = 3 ,
 
 			// max(source1, source2). Invalid when using second source blend modes.
-			max = 5 ,
+			max = 4 ,
 		};
 	}
 
